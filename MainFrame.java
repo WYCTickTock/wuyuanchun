1e61a57121854e7e656b01d650b49b0616dc5906
WYCTickTock <819339175@qq.com>
2016-12-14 11:11:08 +0800

    为MainFrame增加了几个按钮

---
+++ MainFrame.java
@@ -0,0 +1,17 @@
+package wuyuanchun;
+
+import javax.swing.*;
+
+/**
+ * Created by dra on 2016/12/1.
+ */
+public class MainFrame extends JFrame {
+    JButton j1;
+    JButton j2;
+    JButton j3;
+
+
+
+
+
+}
